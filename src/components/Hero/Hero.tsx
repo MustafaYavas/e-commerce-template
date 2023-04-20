e666e2c7f614eef095ea201fc23208e8e1346017
MustafaYavas <mustafayavas40@gmail.com>
2023-04-20 19:03:11 +0300

    Fix responsive bugs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className={styles['hero-container']}>
       <p className={styles['title']}>SPRING SALE</p>
-      <p className={styles['subtitle']}>
+      <p className={`${styles['subtitle']} hidden md:block`}>
         Our products have been renewed! Check out our spring collection.
       </p>
       <Link href="/category/sale">SHOP THE SALE</Link>
