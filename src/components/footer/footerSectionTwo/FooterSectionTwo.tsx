c61616cad32062e89ae9598aa5c9d691c5e447ec
MustafaYavas <mustafayavas40@gmail.com>
2023-04-22 15:55:07 +0300

    Refactor home page

--- src/components/footer/footerSectionTwo/FooterSectionTwo.tsx
+++ src/components/footer/footerSectionTwo/FooterSectionTwo.tsx
@@ -8,7 +8,7 @@ const FooterSectionTwo = () => {
         <div className="flex flex-col justify-center items-center">
           <Icon name="GiCargoShip" color="rgb(31 41 55)" size={52} />
           <h4 className="text-xl my-4 text-slate-600">Priority Shipping</h4>
-          <p className="w-full md:w-3/4 text-start md:text-center text-slate-500 tracking-normal md:tracking-wide">
+          <p className="w-full md:w-3/4 text-center text-slate-500 tracking-normal md:tracking-wide">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
@@ -16,7 +16,7 @@ const FooterSectionTwo = () => {
         <div className="flex flex-col justify-center items-center">
           <Icon name="FaShippingFast" color="rgb(31 41 55)" size={52} />
           <h4 className="text-xl my-4 text-slate-600">Fuss Free Returns</h4>
-          <p className="w-full md:w-3/4 text-start md:text-center text-slate-500 tracking-normal md:tracking-wide">
+          <p className="w-full md:w-3/4 text-center text-slate-500 tracking-normal md:tracking-wide">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
@@ -24,7 +24,7 @@ const FooterSectionTwo = () => {
         <div className="flex flex-col justify-center items-center">
           <Icon name="FaHome" color="rgb(31 41 55)" size={52} />
           <h4 className="text-xl my-4 text-slate-600">In-home Setup Service</h4>
-          <p className="w-full md:w-3/4 text-start md:text-center text-slate-500 tracking-normal md:tracking-wide">
+          <p className="w-full md:w-3/4 text-center text-slate-500 tracking-normal md:tracking-wide">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
