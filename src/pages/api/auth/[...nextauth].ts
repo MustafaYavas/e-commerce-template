644f31570095da89bd2900bede64154aa83cbe70
MustafaYavas <mustafayavas40@gmail.com>
2023-05-20 11:37:45 +0300

    Add next-auth callbacks

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -60,6 +60,37 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user?.email });
+      (session.user as any).id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectMongo();
+
+        const userExists = await User.findOne({ email: profile?.email });
+
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            name: profile?.name?.replace(' ', '').toLowerCase(),
+          });
+        }
+
+        return true;
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        } else {
+          console.log('Unexpected error', error);
+        }
+        return false;
+      }
+    },
+  },
   pages: {
     signIn: '/login',
     error: '/login',
